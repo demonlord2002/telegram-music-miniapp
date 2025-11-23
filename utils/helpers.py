55aceb822b3e372c125d0b8438ba3ce98101331d
demonlord2002 <drdoom2003p@gmail.com>
2025-11-23 18:02:22 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,17 +1,36 @@
-from pyrogram.enums import ChatMemberStatus
+import requests
+import time
 
+def sec_to_min(sec):
+    m = sec // 60
+    s = sec % 60
+    return f"{m:02d}:{s:02d}"
 
-def is_admin(member):
-    return member.status in [
-        ChatMemberStatus.OWNER,
-        ChatMemberStatus.ADMINISTRATOR
-    ]
-
-
-def format_track(track):
+def format_track(track, user):
+    """
+    Nicely formatted message for Telegram
+    """
     return (
-        f"♬ **Started Streaming**\n\n"
+        "♬ **Started Streaming**\n\n"
         f"⋆ **Title:** {track['title']}\n"
-        f"⋆ **Duration:** {track['duration']}\n"
-        f"⋆ **Artists:** {track['artists']}\n"
+        f"⋆ **Artist:** {track['artist']}\n"
+        f"⋆ **Duration:** {sec_to_min(track['duration'])}\n"
+        f"⋆ **Requested by:** {user.first_name}"
     )
+
+def download_audio(url):
+    """
+    Downloads preview audio to /tmp
+    Heroku-safe (ephemeral filesystem)
+    """
+    try:
+        file_path = f"/tmp/audio_{int(time.time())}.mp3"
+        r = requests.get(url)
+
+        with open(file_path, "wb") as f:
+            f.write(r.content)
+
+        return file_path
+
+    except Exception:
+        return None

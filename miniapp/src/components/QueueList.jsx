0ad18a0bf91775a318688dbd18b76432f9d074b3
demonlord2002 <drdoom2003p@gmail.com>
2025-11-23 12:23:34 +0530

    Create QueueList.jsx

---
+++ miniapp/src/components/QueueList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function QueueList() {
+  return (
+    <div className="card">
+      <h3>Queue</h3>
+      <ul>
+        <li>No songs</li>
+      </ul>
+    </div>
+  );
+}

0500906f8761d736f3e8afef4812c3897f4b3122
demonlord2002 <drdoom2003p@gmail.com>
2025-11-23 12:22:49 +0530

    Create PlayerCard.jsx

---
+++ miniapp/src/components/PlayerCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PlayerCard() {
+  return (
+    <div className="card">
+      <h3>Now Playing</h3>
+      <p>No song yet</p>
+    </div>
+  );
+}

21e39fd609a5472e2adebc2f2aadff7bd63c6c6b
demonlord2002 <drdoom2003p@gmail.com>
2025-11-23 18:01:40 +0530

    Update spotify_api.py

--- utils/spotify_api.py
+++ utils/spotify_api.py
@@ -1,38 +1,29 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import os
+import requests
 
-SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
-SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+def search_track(query):
+    """
+    Returns a dict:
+    {
+        "title": "",
+        "artist": "",
+        "duration": "",
+        "thumbnail": "",
+        "audio_url": ""
+    }
+    """
 
-auth = SpotifyClientCredentials(
-    client_id=SPOTIFY_CLIENT_ID,
-    client_secret=SPOTIFY_CLIENT_SECRET,
-)
+    url = f"https://itunes.apple.com/search?term={query}&limit=1"
+    r = requests.get(url).json()
 
-spotify = spotipy.Spotify(auth_manager=auth)
-
-
-def ms_to_time(ms):
-    seconds = int(ms / 1000)
-    minutes = seconds // 60
-    seconds = seconds % 60
-    return f"{minutes:02d}:{seconds:02d}"
-
-
-def search_spotify_track(query):
-    results = spotify.search(q=query, type="track", limit=1)
-
-    if not results["tracks"]["items"]:
+    if not r["resultCount"]:
         return None
 
-    track = results["tracks"]["items"][0]
+    item = r["results"][0]
 
     return {
-        "id": track["id"],
-        "title": track["name"],
-        "artists": ", ".join([a["name"] for a in track["artists"]]),
-        "duration": ms_to_time(track["duration_ms"]),
-        "thumbnail": track["album"]["images"][0]["url"],
-        "preview_url": track["preview_url"],
+        "title": item.get("trackName", "Unknown"),
+        "artist": item.get("artistName", "Unknown"),
+        "duration": item.get("trackTimeMillis", 0) // 1000,
+        "thumbnail": item.get("artworkUrl100"),
+        "audio_url": item.get("previewUrl")  # 30 sec preview only (safe for Heroku)
     }

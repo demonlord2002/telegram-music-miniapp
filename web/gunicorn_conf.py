f69316af09908e9896c87783868a25ffcb1cb2f5
demonlord2002 <drdoom2003p@gmail.com>
2025-11-23 18:34:29 +0530

    Update gunicorn_conf.py

--- web/gunicorn_conf.py
+++ web/gunicorn_conf.py
@@ -1,4 +1,6 @@
-bind = "0.0.0.0:5000"
-workers = 2
+import multiprocessing
+
+bind = "0.0.0.0:" + os.getenv("PORT", "8000")  # Heroku port
+workers = multiprocessing.cpu_count() * 2 + 1
 threads = 4
 timeout = 120

a2c69e338bd3bd6d68b3dda629ba440ff5d556c3
demonlord2002 <drdoom2003p@gmail.com>
2025-11-23 12:24:57 +0530

    Create AdminControls.jsx

---
+++ miniapp/src/components/AdminControls.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AdminControls() {
+  return (
+    <div className="card">
+      <h3>Admin Controls</h3>
+      <button>Skip</button>
+      <button>Stop</button>
+    </div>
+  );
+}

f63e85b4ed0f0cb427b6b39397e0fc069a8092a3
demonlord2002 <drdoom2003p@gmail.com>
2025-11-23 12:24:16 +0530

    Create JoinedUsers.jsx

---
+++ miniapp/src/components/JoinedUsers.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function JoinedUsers() {
+  return (
+    <div className="card">
+      <h3>Users</h3>
+      <p>No users joined yet</p>
+    </div>
+  );
+}

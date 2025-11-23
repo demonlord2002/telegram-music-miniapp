fe767f3cf1abfd7d2c195549598df38d7707e537
demonlord2002 <drdoom2003p@gmail.com>
2025-11-23 18:52:36 +0530

    Update gunicorn_conf.py

--- web/gunicorn_conf.py
+++ web/gunicorn_conf.py
@@ -1,7 +1,9 @@
-import multiprocessing
 import os
 
-bind = "0.0.0.0:" + os.getenv("PORT", "8000")  # Heroku port
-workers = multiprocessing.cpu_count() * 2 + 1
-threads = 4
-timeout = 120
+# Heroku port
+bind = "0.0.0.0:" + os.getenv("PORT", "8000")
+
+# Reduce workers and threads to avoid R14 Memory quota exceeded
+workers = 1        # 1 worker is enough for small apps
+threads = 2        # 2 threads is safe
+timeout = 120      # keep timeout if needed
